bd692280fb95911b83f331b73f1812a6d7a241ec
Raymond Metz <km0852555@otc.edu>
2018-11-19 20:28:18 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var firstTime = true;
+
+
+app.get('/', function (req,res)
+{
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/about', function (req,res)
+{
+    res.sendFile(path.join(__dirname + "/about.html"));
+});
+
+app.listen(3000);
